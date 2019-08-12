24b39da853e8d3de618a777f4068b7691f12ca27
Tommy Huang <tommyhuang999@gmail.com>
2019-08-12 03:41:49 -0400

    Added a function that creates a paragraph with multiple lines

--- functions/components.py
+++ functions/components.py
@@ -27,11 +27,10 @@ def draw_grid(display, start_x, start_y, l, h, space, col, border, offset_l=0, o
         pygame.draw.rect(display, col, (start_x, start_y, l, h), 1)
     display.unlock()
 
-
 # rotates an image by its center
 def rot_center(image, angle):
     """rotate a Surface, maintaining position."""
-    loc = image.get_rect().center  # rot_image is not defined
+    loc = image.get_rect().center
     rot_sprite = pygame.transform.rotate(image, angle)
     rot_sprite.get_rect().center = loc
     return rot_sprite
@@ -45,6 +44,29 @@ def create_text(display, location, text, centered, font, col):
         display.blit(display_text, (location[0] - text_rect[2] // 2, location[1] - text_rect[3] // 2))
     else:
         display.blit(display_text, (location[0], location[1] - text_rect[3] // 2))
+
+
+# creates text with breakpoints every length
+def create_paragraph(display, location, text, font, col, line_height, length):
+    cur_text = ""
+    lines = 0
+    for i in text:
+        new_text = cur_text + i + " "
+        if len(new_text) * 20 > length:
+            # render text to check length
+            new_text = font.render(str(new_text), True, col)
+            if new_text.get_rect()[2] > length:  
+                # display old one if it goes over
+                new_text = font.render(str(cur_text), True, col)
+                display.blit(new_text, (location[0], location[1] + lines * line_height))
+            
+                lines += 1
+                cur_text = ""
+
+        cur_text += i + " "
+    
+    cur_text = font.render(cur_text, True, col)
+    display.blit(cur_text, (location[0], location[1] + lines * line_height))
 
 
 # converts xy pixel value to a grid coord
