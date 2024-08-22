f35affdf948263e8e0c3beecd596753410f42e9d
Tommy Huang <tommyhuang999@gmail.com>
2024-08-22 12:33:17 -0400

    update comment to match the code

--- classes/enemy.py
+++ classes/enemy.py
@@ -27,7 +27,7 @@ class Enemy(object):
         if level > 50:
             self.hp_bonus += (level - 50) * (0.3)
             level = 50
-        self.speed_bonus = 0.006 * level  # max +30% (1.4x)
+        self.speed_bonus = 0.006 * level  # max +30% (1.3x)
         # regen bonus: increases at base rate of +5%. Rate increases after level 20 and level 40
         self.regen_bonus = level * (0.05)  # max + 540% (6.4x)
         if level > 20:
